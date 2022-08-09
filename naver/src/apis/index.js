887aa3fc48754ca4050eb8e49a310c9aa0cf4ffe
younge12 <jey3382@nate.com>
2022-08-09 19:17:27 +0900

    수정

--- naver/src/apis/index.js
+++ naver/src/apis/index.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 
 export const getMovies = async (params) => {
-  const result = await axios.get(
-    "https://openapi.naver.com/v1/search/movie.json",
-    {
-      headers: {
-        "X-Naver-Client-Id": "sjVQsE4KdQWfwMrFpH03",
-        "X-Naver-Client-Secret": "fw7b_GPWIV",
-      },
-      params,
-    }
-  );
+  const result = await axios.get("/v1/search/movie.json", {
+    headers: {
+      "X-Naver-Client-Id": "sjVQsE4KdQWfwMrFpH03",
+      "X-Naver-Client-Secret": "fw7b_GPWIV",
+    },
+    params,
+  });
 
   return result.data;
 };
